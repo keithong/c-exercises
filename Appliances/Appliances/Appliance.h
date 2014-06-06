26eecf81b4815cd68eb4e6d215b4ce8582159e3e
Keith Samson <k.samson@klab.com>
2014-06-06 11:19:18 +0800

    Made accessors nonatomic

--- Appliances/Appliances/Appliance.h
+++ Appliances/Appliances/Appliance.h
@@ -14,8 +14,8 @@
     int voltage;
 }
 
-@property (copy) NSString *productName;
-@property int voltage;
+@property (copy, nonatomic) NSString *productName;
+@property (nonatomic) int voltage;
 
 // The designated initializer
 -(id)initWithProductName:(NSString *)pn;
