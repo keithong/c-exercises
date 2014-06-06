477f730cf7e1867451b7823d170ff2a818950203
Keith Samson <k.samson@klab.com>
2014-06-06 12:09:56 +0800

    Declared some arrays in main

--- VowelMovement/VowelMovement/main.m
+++ VowelMovement/VowelMovement/main.m
@@ -12,9 +12,15 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+            // Create the array of strings to devowelize and a container for new ones
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *oldStrings = [NSArray arrayWithObjects:
+                @"Keithong", @"Bellesome", @"Bonbonme", @"Starfish", nil];
+        NSLog(@"old strings: %@", oldStrings);
+        NSMutableArray *newStrings = [NSMutableArray array];
+        
+        // Create a list of characters that we'll remove from the string
+        NSArray *vowels = [NSArray arrayWithObjects:@"a", @"e", @"i", @"o", @"u", nil];
         
     }
     return 0;
